64cf7ba2c81258d7bcaff14088b50f9cee3ea4f9
Adarsh Thakare <adarshythakare@gmail.com>
2025-11-21 19:15:24 +0530

    clerk initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
   void api.post.getLatest.prefetch();
 
-  return <h1 className="text-3xl text-red-400">Hello world</h1>;
+  return <Button>Use me</Button>;
 }
---
+++ src/app/sync-user/page.tsx
@@ -0,0 +1,44 @@
+import { db } from "@/server/db";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
+import React from "react";
+
+const page = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const client = await clerkClient();
+  console.log("CLERK CLIENT looks like - ", client);
+  const user = await client.users.getUser(userId);
+  console.log("USER looks like - ", user);
+
+  if (!user.emailAddresses[0]?.emailAddress) {
+    return notFound();
+  }
+
+  console.log(db);
+
+  await db.user.upsert({
+    where: {
+      id: userId,
+    },
+    update: {
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    create: {
+      id: userId,
+      emailAddress: user.emailAddresses[0]?.emailAddress ?? "",
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  });
+
+  return <div>page</div>;
+};
+
+export default page;
