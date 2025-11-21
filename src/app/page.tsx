40cb3a1d4bb70420af77f43f3983e78802f232a6
Adarsh Thakare <adarshythakare@gmail.com>
2025-11-21 11:37:32 +0530

    Initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  void api.post.getLatest.prefetch();
+
+  return <h1 className="text-3xl text-red-400">Hello world</h1>;
+}
